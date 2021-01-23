f447c8937675e2fa4691d1548ab188ea6406949c
TANG <tangziqi1226@gmail.com>
2021-01-23 10:25:31 +0100

    English version login ok

--- dao/src/main/java/dao/IRoleDao.java
+++ dao/src/main/java/dao/IRoleDao.java
@@ -6,6 +6,8 @@ import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 public interface IRoleDao {
-    @Select("select* from role where id in (select* from users_role where userId=#{id})")
-    List<Role> findRoleByUserId(String id);
+
+    //根据用户id查询出所有对应的角色
+    @Select("select * from role where id in (select roleId from users_role where userId=#{userId})")
+    List<Role> findRoleByUserId(String userId) throws Exception;
 }
--- service/src/main/java/service/UserServiceImpl.java
+++ service/src/main/java/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package service;
 
-import dao.IUserDao;
+import dao.IUser;
+import domain.Role;
 import domain.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -8,29 +9,29 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service("userService")
-@Transactional
-public class UserServiceImpl implements IUserService{
+public class UserServiceImpl implements IUserService {
     @Autowired
-    private IUserDao userDao;
-
-
+    IUser iUser;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserInfo u=userDao.findByUserName(username);
-        User user=new User(u.getUsername(),"{noop}"+u.getPassword(),getAuthority());
-        return null;
+        UserInfo userInfo=iUser.findByUsername(username);
+        User user = new User(userInfo.getUsername(), "{noop}" + userInfo.getPassword(), userInfo.getStatus() == 0 ? false : true, true, true, true, getAuthority(userInfo.getRoles()));
+        return user;
     }
 
-    List<SimpleGrantedAuthority> getAuthority(){
-        List<SimpleGrantedAuthority> list=new ArrayList<>();
-        list.add(new SimpleGrantedAuthority("ROLE_USER"));
-        return  list;
+    //作用就是返回一个List集合，集合中装入的是角色描述
+    public List<SimpleGrantedAuthority> getAuthority(List<Role> roles) {
+
+        List<SimpleGrantedAuthority> list = new ArrayList<>();
+        for (Role role : roles) {
+            list.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName()));
+        }
+        return list;
     }
 }
